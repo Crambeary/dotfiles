7efa04b2dd8aa17935dd6cb9a9409e9c1b1e1418
Marc <marctotaro94@gmail.com>
2024-03-03 22:19:43 -0600

    Including nvim and i3 from chezmoi copied

---
+++ private_dot_config/i3/scripts/lock.sh
@@ -0,0 +1,1 @@
+i3lock -i ~/Pictures/adrien-olichon-RCAhiGJsUUE-unsplash.png -t
---
+++ private_dot_config/nvim/after/plugin/colors.lua
@@ -0,0 +1,6 @@
+function ColorMyPencils(color)
+	color = color or "miasma"
+	vim.cmd.colorscheme(color)
+	
+end	
+ColorMyPencils()
