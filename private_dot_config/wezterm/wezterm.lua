9928eb05e0c836f819a434723e1556ce028d9009
Marc Totaro <m.totaro@tecplot.com>
2024-10-25 14:45:28 -0500

    added neobrutal-zebar

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -5,5 +5,6 @@ config.color_scheme = "tokyonight_night"
 config.font = wezterm.font("Hack Nerd Font")
 config.window_decorations = "RESIZE"
 config.tab_bar_at_bottom = true
+config.default_prog = { "powershell.exe" }
 
 return config
