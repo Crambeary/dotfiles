68833779d61b4e7fa97f50ea18de75eaec68a969
Marc Totaro <m.totaro@tecplot.com>
2024-10-28 15:58:23 -0500

    wezterm window size doesn't change with font size

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -6,5 +6,6 @@ config.font = wezterm.font("Hack Nerd Font")
 config.window_decorations = "RESIZE"
 config.tab_bar_at_bottom = true
 config.default_prog = { "powershell.exe" }
+config.adjust_window_size_when_changing_font_size = false
 
 return config
