31dc9da75a5ade79ade6c2a723a8fbfa0b5114ff
Marc Totaro <m.totaro@tecplot.com>
2024-11-05 16:23:12 -0600

    Updated Wezterm to use fish on mac

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -5,7 +5,14 @@ config.color_scheme = "tokyonight_night"
 config.font = wezterm.font("Hack Nerd Font")
 config.window_decorations = "RESIZE"
 config.tab_bar_at_bottom = true
-config.default_prog = { "powershell.exe" }
 config.adjust_window_size_when_changing_font_size = false
+-- Platform-specific adjustments (if needed)
+
+if os.getenv("OS") == "Darwin" then
+	-- Mac-specific setting here
+	config.default_prog = { "fish" }
+elseif os.getenv("OS") == "Windows" then
+	config.default_prog = { "powershell.exe" }
+end
 
 return config
