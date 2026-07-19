38ce14f59ff5d589d5983debdef7a195d0743305
Marc Totaro <marctotaro94@gmail.com>
2026-07-19 09:17:37 -0400

    Simplify starship prompt, add cmd_duration; disable Wayland in wezterm
    Starship: swap segmented Powerline format for a flatter
    username/hostname/directory/cmd_duration/git/python line, and enable
    cmd_duration to flag commands over 1s. Wezterm: force enable_wayland
    = false per local machine's display setup.

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -33,6 +33,7 @@ bar.apply_to_config(config,
 
 config.font = wezterm.font("Hack Nerd Font", { weight = 'Regular' })
 config.window_decorations = "RESIZE"
+config.enable_wayland = false
 -- config.tab_bar_at_bottom = true
 config.adjust_window_size_when_changing_font_size = false
 config.front_end = "WebGpu"
